25d500989b5d9e1e5b475b324ddfcfd49d9ca28c
PRANAY MAGGO <E19CSE283@bennett.edu.in>
2021-04-16 21:28:57 +0530

    jUnitTesting

---
+++ src/jUnitTestingPackage/AllTests.java
@@ -0,0 +1,16 @@
+package jUnitTestingPackage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ testAddNumbers.class, testAddStrings.class})
+public class AllTests {
+
+}
+
